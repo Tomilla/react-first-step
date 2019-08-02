4543ff0df953e3fac9277a3ac61e1548a07d2fa6
Gero.Rim <gero.rim@gmail.com>
2019-08-02 15:53:36 +0800

    update
    1. constructor for JS Classes
    2. using setter to modify the property of component, the web-page will re-render automatically
    
    Signed-off-by: Gero.Rim <gero.rim@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <button className="square" onClick={(elem) => {
-        console.debug("You are click one button that its value is", elem);
+      <button className="square" onClick={() => {
+        this.setState({
+          value: "O"
+        });
       }}>
-        {this.props.value}
+        {this.state.value}
       </button>
     );
   }
