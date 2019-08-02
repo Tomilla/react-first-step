b489b72127f52afe932bd9732f3901f353ce8dfb
Gero.Rim <gero.rim@gmail.com>
2019-08-02 15:39:25 +0800

    update
    1. Passing props: "passed a prop” from a parent `Board component` to a child `Square component`.
    
    Signed-off-by: Gero.Rim <gero.rim@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square/>;
+  static renderSquare(i) {
+    return <Square value={i}/>;
   }
 
   render() {
@@ -24,19 +24,19 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {Board.renderSquare(0)}
+          {Board.renderSquare(1)}
+          {Board.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {Board.renderSquare(3)}
+          {Board.renderSquare(4)}
+          {Board.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {Board.renderSquare(6)}
+          {Board.renderSquare(7)}
+          {Board.renderSquare(8)}
         </div>
       </div>
     );
